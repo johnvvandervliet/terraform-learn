43978cc33feca6c1a0f6a6cf93641b01bfcd4b20
John Vandervliet <johnv.yyc@gmail.com>
2023-06-13 12:44:36 -0600

    an update

--- main.tf
+++ main.tf
@@ -2,42 +2,142 @@
 
 # Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-west-3"
     profile = "AdministratorAccess-976225303857"
 }    
 
-variable "cidr_blocks"{
-    description = "cidr blocks of vpc and subnet"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
-}
+variable vpc_cidr_block{}
+variable subnet_cidr_block{}
+variable avail_zone{}
+variable env_prefix{}
+variable my_ip{}
+variable instance_type{}
+variable public_key_location{}
 
-
-
-resource "aws_vpc" "development-vpc"{
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc"{
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name: "${var.env_prefix}-vpc"
     }
 }
-resource "aws_subnet" "dev-subnet-1"{
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "myapp-subnet-1"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name: var.cidr_blocks[1].name
+        Name: "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_route_table" "myapp-route-table"{
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-rtb"
+    }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet"{
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+
+}
+# Terraform will figure out what resource needs to be created first
+resource "aws_internet_gateway" "myapp-igw"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
+}
+
+resource "aws_security_group" "myapp-sg"{
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }    
+        ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+        Name: "${var.env_prefix}-sg"
     }
 }
 
 
-output "dev-vpc-id"{
-    value = aws_vpc.development-vpc.id
-
+data "aws_ami" "latest-amazon-linux-image"{
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+        filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
 }
 
-output "dev-subnet-id"{
-    value = aws_subnet.dev-subnet-1.id
-    
+output "aws_ami_id"{
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip"{
+    value = aws_instance.my-server.public_ip
+}
+
+
+
+resource "aws_key_pair" "ssh-key"{
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "my-server"{
+
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    /* user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                 EOF
+*/
+
+/* or do this */
+    user_data = file("entry-script.sh")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
 }
